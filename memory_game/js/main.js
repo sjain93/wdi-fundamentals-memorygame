01250ee2c221f329f98c9c25e1cecc551037675a
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-02-03 18:03:59 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,8 +21,31 @@ if (cardsInPlay.length === 2) {
 
 */
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank: 'queen',
+suit: 'hearts',
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: 'king',
+suit: 'hearts',
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: 'king',
+suit: 'diamonds',
+cardImage: "images/king-of-diamonds.png"
+},
+];
+
 var cardsInPlay = [];
+var eachCard = [];
 
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -34,11 +57,15 @@ var checkForMatch = function() {
 };
 
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
 };
 
 flipCard(0);
 flipCard(2);
+
+
 
 checkForMatch();
